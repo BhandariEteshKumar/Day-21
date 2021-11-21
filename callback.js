7ef7cd9f8cb929a8623599c422710a455b5d9dc3
BhandariEteshKumar <54309754+BhandariEteshKumar@users.noreply.github.com>
2021-11-21 17:10:51 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,36 @@
+let counter = document.getElementById("counter");
+setTimeout(function () {
+  counter.innerText = "Counter : 1";
+  setTimeout(function () {
+    counter.innerText = "Counter : 2";
+    setTimeout(function () {
+      counter.innerText = "Counter : 3";
+      setTimeout(function () {
+        counter.innerText = "Counter : 4";
+        setTimeout(function () {
+          counter.innerText = "Counter : 5";
+          setTimeout(function () {
+            counter.innerText = "Counter : 6";
+            setTimeout(function () {
+              counter.innerText = "Counter : 7";
+              setTimeout(function () {
+                counter.innerText = "Counter : 8";
+                setTimeout(function () {
+                  counter.innerText = "Counter : 9";
+                  setTimeout(function () {
+                    counter.innerText = "Counter : 10";
+                    setTimeout(function () {
+                      counter.innerText = "Happy Independence day";
+                      counter.style.color =
+                        "liner-gradient(orange,white,green)";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
