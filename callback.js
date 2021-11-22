e2b4472271fb86f7b062b3257425c6baecd7d9bb
BhandariEteshKumar <54309754+BhandariEteshKumar@users.noreply.github.com>
2021-11-22 15:18:48 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,24 +1,24 @@
 let counter = document.getElementById("counter");
 setTimeout(function () {
-  counter.innerText = "Counter : 10";
+  counter.innerText = "Counter: 10";
   setTimeout(function () {
-    counter.innerText = "Counter : 9";
+    counter.innerText = "Counter: 9";
     setTimeout(function () {
-      counter.innerText = "Counter : 8";
+      counter.innerText = "Counter: 8";
       setTimeout(function () {
-        counter.innerText = "Counter : 7";
+        counter.innerText = "Counter: 7";
         setTimeout(function () {
-          counter.innerText = "Counter : 6";
+          counter.innerText = "Counter: 6";
           setTimeout(function () {
-            counter.innerText = "Counter : 5";
+            counter.innerText = "Counter: 5";
             setTimeout(function () {
-              counter.innerText = "Counter : 4";
+              counter.innerText = "Counter: 4";
               setTimeout(function () {
-                counter.innerText = "Counter : 3";
+                counter.innerText = "Counter: 3";
                 setTimeout(function () {
-                  counter.innerText = "Counter : 2";
+                  counter.innerText = "Counter: 2";
                   setTimeout(function () {
-                    counter.innerText = "Counter : 1";
+                    counter.innerText = "Counter: 1";
                     setTimeout(function () {
                       counter.innerText = "Happy Independence day";
                       document.getElementById("image").style =
